50d90e8fcff64d6b78e8040816979b6b9b8d3de6
tommyrharper <tommyrharper@gmail.com>
2020-06-23 12:33:43 +0100

    Replace app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import './App.css';
+import Projects from './components/Projects';
+import Navigation from './components/Navigation';
+import Home from './components/Home'
+import MissionCtrl from './components/projects/MissionCtrl'
+import Bank from './components/projects/Bank'
+import Thermostat from './components/projects/Thermostat'
+import Bowling from './components/projects/Bowling'
+import Acebook from './components/projects/Acebook'
+import Chitter from './components/projects/Chitter'
+import RPS from './components/projects/RPS'
+import Takeaway from './components/projects/Takeaway';
+import Airport from './components/projects/Airport';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+          <div>
+            <Navigation/>
+              <Switch> 
+                <Route exact path="/" component={Home}/>
+                <Route path="/Projects" component={Projects}/>
+                <Route exact path="/missionctrl" component={MissionCtrl}/>
+                <Route exact path="/bank" component={Bank}/>
+                <Route exact path="/thermostat" component={Thermostat}/>
+                <Route exact path="/bowling" component={Bowling}/>
+                <Route exact path="/acebook" component={Acebook}/>
+                <Route exact path="/chitter" component={Chitter}/>
+                <Route exact path="/rps" component={RPS}/>
+                <Route exact path="/takeaway" component={Takeaway}/>
+                <Route exact path="/airport" component={Airport}/>
+              </Switch>
+          </div> 
+      </div>
+    ) 
+  }
 }
 
-export default App;
+export default withRouter(App);
