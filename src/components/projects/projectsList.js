693b7c2cdfd9a3078ada66a5301b4c3dde0a1261
tommyrharper <tommyrharper@gmail.com>
2020-06-23 12:36:49 +0100

    Add projectsList.js

---
+++ src/components/projects/projectsList.js
@@ -0,0 +1,13 @@
+const projectsList = [
+  "MissionCtrl",
+  "Bank",
+  "Thermostat",
+  "Bowling-Scorecard",
+  "Acebook-myspace",
+  "Twitter",
+  "Rock-Paper-Scissors",
+  "Takeaway-Service",
+  "Airport"
+]
+
+export default projectsList;
