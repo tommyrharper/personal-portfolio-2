bb0135a2bd5f60a70dbf217f211389e98a722cdc
tommyrharper <tommyrharper@gmail.com>
2020-06-23 12:37:11 +0100

    Add Thermostat.js

---
+++ src/components/projects/Thermostat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Thermostat extends React.Component {
+  render() {
+    return(
+    <div>
+      <h1>Thermostat</h1>
+      <h4><a href="http://toms-amazing-thermostat.surge.sh">Try it out!</a></h4>
+      <h4><a href="https://github.com/tommyrharper/thermostat">Github</a></h4>
+    </div>
+    ) 
+  }
+}
+
+export default Thermostat;
